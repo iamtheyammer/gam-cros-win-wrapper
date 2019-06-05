60c84ac0aeda8b0dcd78cf54f59ac78d54bddcd1
I Yam Who I Yam <32084206+iamtheyammer@users.noreply.github.com>
2019-06-04 21:53:56 -0700

    Use new updates system, release Alpha 1.1.0

--- ChromebookGUI/ChromebookGUI/App.xaml.cs
+++ ChromebookGUI/ChromebookGUI/App.xaml.cs
@@ -43,7 +43,7 @@ namespace ChromebookGUI
             {
                 if (Debug.IsDebugMode() == true)
                 {
-                    throw;
+                    throw err;
                 }
                 Debug.CaptureException(err);
 
